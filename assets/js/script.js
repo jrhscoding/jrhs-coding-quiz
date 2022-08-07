d6cd9dda5bbae696784228b6ba6ab7b0a753262e
joseph <j.hernandez.92189@gmail.com>
2022-08-07 14:20:03 -0700

    Added answers to the first question, gave correct answer id of 'correct'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var questionContainer = document.querySelector(".question-container");
+var startButton = document.querySelector("#start");
 
 questionsArr = [
     {
@@ -24,15 +25,39 @@ var questionTwo = questionsArr[1].question;
 
 var questionThree= questionsArr[2].question;
 
+// function to return the question onto the page
 var printQuestion = function() {
     const printContainer = document.createElement("div");
     printContainer.className = "Questions";
     printContainer.innerHTML = "<h3>" + questionOne + "</h3>";
     console.log(printContainer);
+
+
+    const answerWrapper = document.createElement("ul");
+    answerWrapper.className = "answer-wrapper";
+    printContainer.appendChild(answerWrapper);
+
+    const listQuestion1 = document.createElement("li");
+    listQuestion1.className = "answers";
+    listQuestion1.id = "correct";
+    listQuestion1.textContent = questionsArr[0].correct;
+    answerWrapper.appendChild(listQuestion1);
+
+    const listQuestion2 = document.createElement("li");
+    listQuestion2.className = "answers";
+    listQuestion2.textContent = questionsArr[0].incorrectOne;
+    answerWrapper.appendChild(listQuestion2);
+   
+   
+   
     return printContainer;
+
 };
 
-questionContainer.appendChild(printQuestion());
+// added event listener to print question onto page after clicking start
+startButton.addEventListener("click", function() {
+    questionContainer.appendChild(printQuestion());
+});
 
 
 // step1 creat div containing question as h2 and correct and incorrect as li's
